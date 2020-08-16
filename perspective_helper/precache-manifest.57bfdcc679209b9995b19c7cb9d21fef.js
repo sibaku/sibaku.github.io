2484d4e892f7e0d62b0970c3e9c0b6cc7b74d86d
sibaku <sibaku@gmail.com>
2020-08-16 22:13:57 +0200

    added perspective helper project

---
+++ perspective_helper/precache-manifest.57bfdcc679209b9995b19c7cb9d21fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88b89fd004738d2181bf013fc5ae7c8",
+    "url": "/perspective_helper/index.html"
+  },
+  {
+    "revision": "b599d865f2f169e4ab71",
+    "url": "/perspective_helper/static/css/main.620acb9e.chunk.css"
+  },
+  {
+    "revision": "50c011deb629574bb329",
+    "url": "/perspective_helper/static/js/2.85cb75f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/perspective_helper/static/js/2.85cb75f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b599d865f2f169e4ab71",
+    "url": "/perspective_helper/static/js/main.a3eac73c.chunk.js"
+  },
+  {
+    "revision": "595fbcda0ad01e0400ac",
+    "url": "/perspective_helper/static/js/runtime-main.b763fc01.js"
+  }
+]);
